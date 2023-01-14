9b5f0f5a272432fa5e1414e404cd6d05782f2dd4
Aamir Ashraf <aamirnasa97@gmail.com>
2023-01-14 22:59:51 +0530

    almost registration form is complete

--- enroll/views.py
+++ enroll/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
 def showdata(request):
     if(request.method=='POST'):
         fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm=StudentRegistration() #this for clearing the form
+            #we can also use the shortcut fm.save() directly instead of doing these
+            #kinds of the cleaned data concept this is just a brilliant concept to deals with it
+            #the below one is that if you want to save the particular data this below approch will best
+            #if we want to save all data then we should use .save() function
+            #  nm=fm.cleaned_data['name']
+            #  em=fm.cleaned_data['email']
+            #  pw=fm.cleaned_data['password']
+            #  reg=User(name=nm,email=em,password=pw)
+            #  reg.save()
     else:
         fm=StudentRegistration();
     return render(request,'enroll/addandshow.html',{'form':fm})
